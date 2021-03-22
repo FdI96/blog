3af67c4905bf436702f12d7a6978fe2090b54857
FdI96 <federicolopezce@gmail.com>
2021-03-22 19:23:09 -0300

    Create a comment folder with partial _comment file and a _form for the comments and changed article model to delete all the comments for each article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
-  has_many :comments
-
+  has_many :comments, dependent: :delete_all
+  # To make this works, on the destroy action, we need to also delete all the comments that belongs to that article
+  # It affects the data integrity so we added the "dependent: :delete_all".
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
